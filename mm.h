136455bfac942dfe0134391b37cf277621d30839
Muhammad Usman Zaman <usman.zaman@afiniti.com>
2020-12-15 05:49:32 +0500

    Meta data block strcut addition. Added macros to loop through the meta data blocks

--- mm.h
+++ mm.h
@@ -2,6 +2,22 @@
 
 #define MM_MAX_STRUCT_NAME 32
 
+#include <stddef.h> /*for size_t*/
+
+
+typedef enum{
+    MM_FALSE,
+    MM_TRUE
+} vm_bool_t;
+
+typedef struct _block_meta_data{
+    vm_bool_t is_free;
+    uint32_t block_size;
+    uint32_t offset;    /* offset from the start of the page */
+    struct _block_meta_data *prev_block;
+    struct _block_meta_data *next_block;
+} block_meta_data_t;
+
 typedef struct _vm_page_family{
 	char struct_name[MM_MAX_STRUCT_NAME];
 	uint32_t struct_size;	
@@ -25,3 +41,20 @@ typedef struct _vm_page_for_families{
 #define ITERATE_PAGE_FAMILIES_END(vm_page_for_families_ptr, curr)        }}
 
 vm_page_family_t * lookup_page_family_by_name (char *struct_name);
+
+#define offset_of(container_structure, field_name)                          \
+	((size_t)(&(((container_structure *)0)->field_name)))
+
+#define MM_GET_PAGE_FROM_META_BLOCK(block_meta_data_ptr)                    \
+	((void*)((char *)(block_meta_data_ptr) - ((block_meta_data_ptr)->offset)))
+
+#define NEXT_META_BLOCK(block_meta_data_ptr)                                \
+	((block_meta_data_ptr)->next_block)
+
+#define PREV_META_BLOCK(block_meta_data_ptr)                                \
+	((block_meta_data_ptr)->prev_block)
+
+#define NEXT_META_BLOCK_BY_SIZE(block_meta_data_ptr)                        \
+	((block_meta_data_ptr*)                                                 \
+	((char *)((block_meta_data_ptr) + 1) +                                  \
+	((block_meta_data_ptr)->block_size)))
